98c009d05849b5985342d6e9cef80d619af6774a
feelkbtdongil <feelkbt.dongil@gmail.com>
2024-06-29 15:44:06 +0900

    Add files via upload

---
+++ assets/loading-plugin.b6f9a4d8.js
@@ -0,0 +1,1 @@
+import{b as a,c as e}from"./index.7f77393b.js";var i=a(async()=>{e.setDefaults({delay:0,message:"\uB85C\uB529\uC911...!!",spinnerSize:40})});export{i as default};
---
+++ assets/loadingBar-plugin.91e3e88c.js
@@ -0,0 +1,1 @@
+import{b as a,d as o}from"./index.7f77393b.js";var r=a(async()=>{o.setDefaults({color:"purple",size:"5px"})});export{r as default};
